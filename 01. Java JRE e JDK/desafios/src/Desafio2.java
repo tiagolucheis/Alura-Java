844c90ad5d7feae3a286099d2355d15d082fc067
Tiago Lucheis <tiagolucheis@gmail.com>
2022-01-09 10:50:00 -0300

    Course contents
    Added the Course Contents.

---
+++ 01. Java JRE e JDK/desafios/src/Desafio2.java
@@ -0,0 +1,17 @@
+
+public class Desafio2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int fatorial = 1;
+		
+		for (int n = 1; n <= 10; n++) {
+			fatorial *= n;
+		}
+		
+		System.out.println(fatorial);
+		
+	}
+
+}
