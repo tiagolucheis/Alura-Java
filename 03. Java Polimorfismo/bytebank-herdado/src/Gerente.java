79a41d388ae2922dbdff1d9535f4d6b3a30ff1b5
Tiago Lucheis <tiagolucheis@gmail.com>
2022-01-14 08:01:06 -0300

    Heritance Exercise Added
    Employee example for Heritance Class

--- 03. Java Polimorfismo/bytebank-herdado/src/Gerente.java
+++ 03. Java Polimorfismo/bytebank-herdado/src/Gerente.java
@@ -1,46 +1,22 @@
+public class Gerente extends Funcionario {
 
-public class Gerente extends Funcionario{
-
-	private String nome;
-	private String cpf;
-	private double salario;
 	private int senha;
+	
+	@Override
+	public double getBonificacao() {
+	    return super.getBonificacao() + super.getSalario();
+	}
 	
 	public void setSenha(int senha) {
 		this.senha = senha;
 	}
-	
+
 	public boolean autentica(int senha) {
-		if(this.senha == senha) {
+		if (this.senha == senha) {
 			return true;
 		} else {
 			return false;
 		}
 	}
-	
-	public double getBonificacao() {
-		return this.salario;
-	}
-	
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public String getCpf() {
-		return cpf;
-	}
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-	public double getSalario() {
-		return salario;
-	}
-	public void setSalario(double salario) {
-		this.salario = salario;
-	}
-	
-	
-	
+
 }
