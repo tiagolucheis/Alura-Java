67caef53fc91f0f6971366e4ed8874b796ad0745
Tiago Lucheis <tiagolucheis@gmail.com>
2022-01-15 19:33:10 -0300

    Java Polimorfism Final Project
    Final Project for the Java Polimosfism course

--- 03. Java Polimorfismo/bytebank-herdado-conta/src/TesteContas.java
+++ 03. Java Polimorfismo/bytebank-herdado-conta/src/TesteContas.java
@@ -1,16 +1,20 @@
 
 public class TesteContas {
+
 	public static void main(String[] args) {
+					
+		ContaCorrente cc = new ContaCorrente(111, 111);
+		cc.deposita(100.0);
 		
-		ContaCorrente cc = new ContaCorrente(111,222);
-		cc.deposita(100.00);
+		ContaPoupanca cp = new ContaPoupanca(222, 222);
+		cp.deposita(200.0);
 		
-		ContaPoupanca cp = new ContaPoupanca(222,222);
-		cp.deposita(200.00);
-		
-		cc.transfere(10.00,cp);
+		cc.transfere(10.0, cp);
 		
 		System.out.println("CC: " + cc.getSaldo());
 		System.out.println("CP: " + cp.getSaldo());
+
+
 	}
+
 }
--- 03. Java Polimorfismo/bytebank-herdado/src/TesteGerente.java
+++ 03. Java Polimorfismo/bytebank-herdado/src/TesteGerente.java
@@ -1,7 +1,10 @@
 
 public class TesteGerente {
 
-	public static void main(String[] args) {		
+	public static void main(String[] args) {
+		
+		Autenticavel referencia = new Cliente();
+		
 		Gerente g1 = new Gerente();
 		g1.setNome("Marco");
 		g1.setCpf("235568413");
